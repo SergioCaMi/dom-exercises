2a21851e909565ea9838d13bfc8a103e21c30ef8
SergioCaMi <sergiocami84@gmail.com>
2025-03-28 10:41:51 +0100

    fin ejercicios

--- ex2.js
+++ ex2.js
@@ -13,5 +13,9 @@ function asociamePorFavor() {
 
 function calcularLetrasTitulo() {
   // 1. Accede a la propiedad textContent del nodo h1. Guarda esa información en una variable
+  const textH1 = document.querySelector("h1").textContent;
   // 2. Usa el método adecuado para obtener el número de carácteres del string en textContent. Pon ese valor en el textContent del #panel
+  document.getElementById("panel").style.fontSize = "48px";
+  console.log(document.getElementById("panel").textContent=textH1.length+" carácteres en el h1.") ;
+
 }
