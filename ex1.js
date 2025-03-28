5bcb29a7bad41dd6bcdf6617828e3f13527416df
SergioCaMi <sergiocami84@gmail.com>
2025-03-28 09:32:20 +0100

    detalles

--- ex1.js
+++ ex1.js
@@ -9,22 +9,35 @@
  * Cambia el título de la página a rojo usando un estilo en línea
  */
 
+const title = document.querySelector("h1");
+const box2 = document.getElementById("caja-2");
+const pIntro = document.querySelector("p.intro");
+const lastBox = document.getElementById('ultimo');
+const secondLast = document.querySelector("p:nth-last-of-type(2)");
+
+
+title.style.color = "red";  
 /**
  * Cambia el tamaño del título de la página a 48px usando un estilo en línea
  */
-
+title.style.fontSize = "48px";
 /**
  * Muestra por console.log el texto de la caja 2
  */
 
+console.log(box2.textContent)
 /**
  * Cambia el color del borde del párrafo p.intro a verde usando un estilo en línea
  */
+pIntro.style.borderColor = "green";
 
 /**
  * Elimina la clase "estilo-3" de la última caja.
  * Ayuda: https://www.w3schools.com/howto/howto_js_remove_class.asp
  */
+
+
+lastBox.classList.remove('estilo-3');
 
 /**
  * Añade la clase CSS "ocultar" al penúltimo párrafo. La última cita debería desaparecer. Compruebalo con el inspector de elementos. 
@@ -33,3 +46,4 @@
  * BONUS: Haz el ejercicio sin modificar el ex1.html
  
  */
+secondLast.classList.add("ocultar");
--- ex3.js
+++ ex3.js
@@ -3,6 +3,7 @@
  */
 
 let numeroClicsPacman = 0;
+let numeroClicsGhost = 0;
 
 function clicPacman() {
   // incremento la variable global clicsPacman
@@ -10,4 +11,10 @@ function clicPacman() {
 
   // actualizo el text content del nodo adecuado
   document.querySelector("#clics-pacman").textContent = numeroClicsPacman;
+}
+
+
+function clicGhost(){
+  numeroClicsGhost++;
+  document.querySelector("#clics-fantasma").textContent = numeroClicsGhost;
 }
